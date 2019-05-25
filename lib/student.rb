7aa6e63a88a964e8bc4eab6189b50d91c1922dda
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-05-25 09:53:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ def self.create_table
       CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        album TEXT
+         TEXT
         )
         SQL
     DB[:conn].execute(sql) 
