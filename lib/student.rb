f4f8e84742f8a6adbfc000f3f1b047ca297a48b9
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-05-25 09:56:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,10 +14,21 @@ def self.create_table
       CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-         TEXT
+        grade TEXT
         )
         SQL
     DB[:conn].execute(sql) 
-end 
+end
+
+def self.drop_table
+        sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+end
 
 end
