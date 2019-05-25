a5e2a5da088a8598f33284a7cb04fe6227356b4a
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-05-25 10:06:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,12 @@ def self.drop_table
 end
 
 def save
-  
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
 end
+
 end
